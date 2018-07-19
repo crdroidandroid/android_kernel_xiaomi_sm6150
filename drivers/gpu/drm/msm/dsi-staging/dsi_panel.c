d012fec45c30b2e2253f567765622ccbf029837c
Danny Baumann <dannybaumann@web.de>
2018-07-19 09:41:23 +0200

    drm: msm: dsi-staging: Add support for high brightness mode (HBM)
    Author: Arian <arian.kulmer@web.de>
    Date:   Wed Jun 24 15:44:22 2020 +0200
    
        drm: msm: Use type_map array index 0 for invalid hbm values
    
        Change-Id: I8b29ad7227be9f01f945af136747a5924b0b628a
    
    Change-Id: Id133e30c7bf645386053c42d4d121a053b03bac7
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    
    * Adapt to sweet
    
    Commit ref:
    https://github.com/ArianK16a/android_kernel_xiaomi_sm6150/commit/9c5639db7361c53b64c0725d19306e3b28cbe9b2
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -6270,6 +6270,10 @@ int dsi_panel_enable(struct dsi_panel *panel)
 
 	mutex_unlock(&panel->panel_lock);
 	pr_info("[SDE] %s: DSI_CMD_SET_ON\n", __func__);
+
+	if (panel->hbm_mode)
+		dsi_panel_apply_hbm_mode(panel);
+
 	return rc;
 }
 
@@ -6449,6 +6453,29 @@ int dsi_panel_post_unprepare(struct dsi_panel *panel)
 	}
 error:
 	mutex_unlock(&panel->panel_lock);
+	return rc;
+}
+
+int dsi_panel_apply_hbm_mode(struct dsi_panel *panel)
+{
+	static const enum dsi_cmd_set_type type_map[] = {
+		DSI_CMD_SET_DISP_HBM_OFF,
+		DSI_CMD_SET_DISP_HBM_ON
+	};
+
+	enum dsi_cmd_set_type type;
+	int rc;
+
+	if (panel->hbm_mode >= 0 &&
+		panel->hbm_mode < ARRAY_SIZE(type_map))
+		type = type_map[panel->hbm_mode];
+	else
+		type = type_map[0];
+
+	mutex_lock(&panel->panel_lock);
+	rc = dsi_panel_tx_cmd_set(panel, type);
+	mutex_unlock(&panel->panel_lock);
+
 	return rc;
 }
 
