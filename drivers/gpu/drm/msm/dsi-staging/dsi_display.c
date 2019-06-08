182a0f52f3158467f58c89d6e65c0a135fa8309c
Adrian Salido <salidoa@google.com>
2019-06-07 18:05:39 -0700

    drm/msm/dsi-staging: fix power check in idle hints
    The current checks only work if Always On Display is enabled. In that
    case power_mode is used to indicate when it's going in and out of Low
    Power Mode, however the power mode is not properly maintained otherwise.
    Instead modify the check to only use power_mode to check LP mode and use
    internal initialized state to indicate if panel is really on.
    
    Bug: 129355653
    Change-Id: I5e93bb4ee88cd8edaf151bbc04d930cc9e833d30
    Signed-off-by: Adrian Salido <salidoa@google.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -66,6 +66,12 @@ struct dsi_display *get_primary_display(void)
 }
 EXPORT_SYMBOL(get_primary_display);
 
+static inline bool is_lp_mode(int power_mode)
+{
+	return power_mode == SDE_MODE_DPMS_LP1 ||
+			power_mode == SDE_MODE_DPMS_LP2;
+}
+
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -1269,8 +1275,7 @@ int dsi_display_set_power(struct drm_connector *connector,
 		drm_notifier_call_chain(DRM_EVENT_BLANK, &g_notify_data);
 		break;
 	case SDE_MODE_DPMS_ON:
-		if (display->panel->power_mode == SDE_MODE_DPMS_LP1 ||
-			display->panel->power_mode == SDE_MODE_DPMS_LP2) {
+		if (is_lp_mode(display->panel->power_mode)) {
 			drm_notifier_call_chain(DRM_EARLY_EVENT_BLANK, &g_notify_data);
 			rc = dsi_panel_set_nolp(display->panel);
 			drm_notifier_call_chain(DRM_EVENT_BLANK, &g_notify_data);
@@ -6551,7 +6556,8 @@ void dsi_display_set_idle_hint(void *dsi_display, bool is_idle)
 	if (unlikely(!display || !display->panel))
 		return;
 
-	if (display->panel->power_mode != DRM_MODE_DPMS_ON)
+	if (!dsi_panel_initialized(display->panel) ||
+	    is_lp_mode(display->panel->power_mode))
 		return;
 
 	if (is_idle)
