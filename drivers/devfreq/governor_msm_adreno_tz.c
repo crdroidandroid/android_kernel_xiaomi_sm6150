cdc02b029826b87adcae0325841619b05851163e
ZyCromerZ <neetroid97@gmail.com>
2020-01-29 21:10:03 +0700

    adrenoboost: fix max value adrenoboost
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -100,7 +100,7 @@ static ssize_t adrenoboost_save(struct device *dev,
 {
 	int input;
 	sscanf(buf, "%d ", &input);
-	if (input < 0 || input > 4) {
+	if (input < 0 || input > 3) {
 		if (input < 0){
 			adrenoboost = 0;
 		} else {
