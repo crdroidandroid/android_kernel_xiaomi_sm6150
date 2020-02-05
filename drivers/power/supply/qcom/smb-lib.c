124256794a128eae129bc5297b6fc71f90f99a9d
Pig <pig.priv@gmail.com>
2020-02-05 15:19:10 +0100

    power: qcom: Import Xiaomi changes
    Change-Id: I8a645d959d78dafa6ff1826d88e9d275606b4d40
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -2535,7 +2535,13 @@ int smblib_get_prop_die_health(struct smb_charger *chg,
 
 #define SDP_CURRENT_UA			500000
 #define CDP_CURRENT_UA			1500000
+#ifdef CONFIG_MACH_XIAOMI_SDMMAGPIE
+#define DCP_CURRENT_UA			1600000
+#elif defined (CONFIG_MACH_XIAOMI_SM6150)
+#define DCP_CURRENT_UA			2000000
+#else
 #define DCP_CURRENT_UA			1500000
+#endif
 #define HVDCP_CURRENT_UA		3000000
 #define TYPEC_DEFAULT_CURRENT_UA	900000
 #define TYPEC_MEDIUM_CURRENT_UA		1500000
