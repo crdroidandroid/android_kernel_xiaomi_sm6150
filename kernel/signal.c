63d3efd8afc1ef97f8956a933f7f0fd55e97eaf1
Ashwini Oruganti <ashfall@google.com>
2020-04-19 01:00:34 -0700

    Revert "mm: oom_kill: reap memory of a task that receives SIGKILL"
    This reverts commit 724adb443397221ef25afee5183466aa9ae49703.
    
    Reason for revert: The changes introduced in this commit are causing an
    undesirable SELinux denial as a side-effect. Additionally, it appears
    (http://b/130391762) that we do not enable the functionality that this
    commit adds. Reverting the commit fixes the SELinux denial bug.
    
    Bug: 138594811
    Change-Id: Ie440e290dc89b72c46de2194da0275910c03a66d
    Signed-off-by: Ashwini Oruganti <ashfall@google.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -42,8 +42,6 @@
 #include <linux/cn_proc.h>
 #include <linux/compiler.h>
 #include <linux/posix-timers.h>
-#include <linux/oom.h>
-#include <linux/capability.h>
 #include <linux/cgroup.h>
 
 #define CREATE_TRACE_POINTS
@@ -1346,11 +1344,8 @@ int group_send_sig_info(int sig, struct siginfo *info, struct task_struct *p)
 	ret = check_kill_permission(sig, info, p);
 	rcu_read_unlock();
 
-	if (!ret && sig) {
+	if (!ret && sig)
 		ret = do_send_sig_info(sig, info, p, true);
-		if (capable(CAP_KILL) && sig == SIGKILL)
-			add_to_oom_reaper(p);
-	}
 
 	return ret;
 }
