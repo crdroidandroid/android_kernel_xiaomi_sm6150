ab40742a890720053786868f519530eac4f64f39
Neal Cardwell <ncardwell@google.com>
2020-09-10 15:35:34 -0400

    UPSTREAM: tcp: simplify tcp_set_congestion_control(): Always reinitialize
    Now that the previous patches ensure that all call sites for
    tcp_set_congestion_control() want to initialize congestion control, we
    can simplify tcp_set_congestion_control() by removing the reinit
    argument and the code to support it.
    
    Change-Id: I2a34569215d029c1d06fc1eafe3a65f9acd32b7b
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yuchung Cheng <ycheng@google.com>
    Acked-by: Kevin Yang <yyd@google.com>
    Cc: Lawrence Brakmo <brakmo@fb.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2671,7 +2671,7 @@ static int do_tcp_setsockopt(struct sock *sk, int level,
 
 		lock_sock(sk);
 		sk_defer_free_flush(sk);
-		err = tcp_set_congestion_control(sk, name, true, true,
+		err = tcp_set_congestion_control(sk, name, true,
 						 ns_capable(sock_net(sk)->user_ns,
 							    CAP_NET_ADMIN));
 		release_sock(sk);
