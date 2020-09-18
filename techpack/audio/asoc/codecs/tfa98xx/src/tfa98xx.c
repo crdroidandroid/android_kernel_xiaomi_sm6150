27d10b74a0840c5ec9f53aef356239488e3379d6
PIPIPIG233666 <pig.priv@gmail.com>
2020-09-17 22:48:07 -0400

    tfa98xx: Promote send_tfa_cal_apr out of debugfs codes
    Change-Id: I95be412e79dfdd9e29785ac1022fcc7664cdcdff

--- techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
@@ -369,6 +369,16 @@ static void tfa98xx_inputdev_unregister(struct tfa98xx *tfa98xx)
 	__tfa98xx_inputdev_check_register(tfa98xx, true);
 }
 
+#ifdef CONFIG_TFA_NON_DSP_SOLUTION
+extern int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead);
+#else
+int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead)
+{
+	pr_info("this function is empty!!!\n");
+	return 0;
+}
+#endif
+
 #ifdef CONFIG_DEBUG_FS
 /* OTC reporting
  * Returns the MTP0 OTC bit value
@@ -727,15 +737,6 @@ static ssize_t tfa98xx_dbgfs_fw_state_get(struct file *file,
 	return simple_read_from_buffer(user_buf, count, ppos, str, strlen(str));
 }
 
-#ifdef CONFIG_TFA_NON_DSP_SOLUTION
-extern int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead);
-#else
-int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead)
-{
-	pr_info("this function is empty!!!\n");
-	return 0;
-}
-#endif
 static ssize_t tfa98xx_dbgfs_rpc_read(struct file *file,
 	char __user *user_buf, size_t count,
 	loff_t *ppos)
