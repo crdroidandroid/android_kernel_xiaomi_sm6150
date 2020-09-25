b35b449e776d78bb1536e1380da4d3e1b6676be6
Daniel Mentz <danielmentz@google.com>
2020-09-24 17:01:20 -0700

    BACKPORT: ANDROID: Disable kthread delayed work fp check in CFI builds
    With non-canonical CFI, LLVM generates jump table entries for external
    symbols in modules and as a result, a function pointer passed from a
    module to the core kernel will have a different address.
    
    Disable the warning for now.
    
    Bug: 145210207
    Signed-off-by: Daniel Mentz <danielmentz@google.com>
    Change-Id: I576a07206a465902773481e51a84529f0ac2e84b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -898,8 +898,10 @@ void __kthread_queue_delayed_work(struct kthread_worker *worker,
 	struct timer_list *timer = &dwork->timer;
 	struct kthread_work *work = &dwork->work;
 
+#ifndef CONFIG_CFI_CLANG
 	WARN_ON_ONCE(timer->function != kthread_delayed_work_timer_fn ||
 		     timer->data != (unsigned long)dwork);
+#endif
 
 	/*
 	 * If @delay is 0, queue @dwork->work immediately.  This is for
