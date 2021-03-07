daa4b4ce21c2ebc04df3218f3ed6b4719c9a38e5
Pig <pig.priv@gmail.com>
2021-03-07 08:46:51 +0800

    dma-buf: Fix a8a11f298c527 mismerge
    Change-Id: I38ae1e42229963a0251c9f06f3ea186c2c147147

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -105,7 +105,6 @@ out:
 static void dma_buf_release(struct dentry *dentry)
 {
 	struct dma_buf *dmabuf;
-	struct dentry *dentry = file->f_path.dentry;
 
 	dmabuf = dentry->d_fsdata;
 
@@ -137,7 +136,6 @@ static void dma_buf_release(struct dentry *dentry)
 
 	module_put(dmabuf->owner);
 	dmabuf_dent_put(dmabuf);
-	return 0;
 }
 
 static const struct dentry_operations dma_buf_dentry_ops = {
