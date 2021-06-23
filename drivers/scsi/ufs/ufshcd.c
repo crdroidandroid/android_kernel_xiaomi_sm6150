307dfab261e9e785062f05d7b133e2e8b6c476cb
Jebaitedneko <Jebaitedneko@gmail.com>
2021-06-23 06:32:20 +0530

    scsi: ufs: disable caf wb backport
    dropping this in favor of samsung turbo write
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -369,6 +369,12 @@ ufs_get_pm_lvl_to_link_pwr_state(enum ufs_pm_level lvl)
 	return ufs_pm_lvl_states[lvl].link_state;
 }
 
+#if 1
+static inline void ufshcd_wb_toggle_flush(struct ufs_hba *hba)
+{
+	return;
+}
+#else
 static inline void ufshcd_wb_toggle_flush(struct ufs_hba *hba)
 {
 	/*
@@ -382,7 +388,14 @@ static inline void ufshcd_wb_toggle_flush(struct ufs_hba *hba)
 	else
 		ufshcd_wb_buf_flush_disable(hba);
 }
+#endif
 
+#if 1
+static inline void ufshcd_wb_config(struct ufs_hba *hba)
+{
+	return;
+}
+#else
 static inline void ufshcd_wb_config(struct ufs_hba *hba)
 {
 	int ret;
@@ -402,6 +415,7 @@ static inline void ufshcd_wb_config(struct ufs_hba *hba)
 			"%s: En Write-Booster flush during H8: failed : %d\n",
 			__func__, ret);
 }
+#endif
 
 static inline void ufshcd_set_card_online(struct ufs_hba *hba)
 {
@@ -7048,6 +7062,12 @@ out:
 				__func__, err);
 }
 
+#if 1
+static bool ufshcd_wb_sup(struct ufs_hba *hba)
+{
+	return false;
+}
+#else
 static bool ufshcd_wb_sup(struct ufs_hba *hba)
 {
 #if defined(CONFIG_UFSTW)
@@ -7063,7 +7083,14 @@ static bool ufshcd_wb_sup(struct ufs_hba *hba)
 		  (hba->dev_info.b_wb_buffer_type
 		   || hba->dev_info.wb_config_lun));
 }
+#endif
 
+#if 1
+static int ufshcd_wb_ctrl(struct ufs_hba *hba, bool enable)
+{
+	return 1;
+}
+#else
 static int ufshcd_wb_ctrl(struct ufs_hba *hba, bool enable)
 {
 	int ret;
@@ -7091,7 +7118,14 @@ static int ufshcd_wb_ctrl(struct ufs_hba *hba, bool enable)
 
 	return ret;
 }
+#endif
 
+#if 1
+static int ufshcd_wb_toggle_flush_during_h8(struct ufs_hba *hba, bool set)
+{
+	return 1;
+}
+#else
 static int ufshcd_wb_toggle_flush_during_h8(struct ufs_hba *hba, bool set)
 {
 	int val, ret;
@@ -7112,7 +7146,14 @@ static int ufshcd_wb_toggle_flush_during_h8(struct ufs_hba *hba, bool set)
 	return ret;
 
 }
+#endif
 
+#if 1
+static int ufshcd_wb_buf_flush_enable(struct ufs_hba *hba)
+{
+	return 0;
+}
+#else
 static int ufshcd_wb_buf_flush_enable(struct ufs_hba *hba)
 {
 	int ret;
@@ -7129,7 +7170,14 @@ static int ufshcd_wb_buf_flush_enable(struct ufs_hba *hba)
 
 	return ret;
 }
+#endif
 
+#if 1
+static int ufshcd_wb_buf_flush_disable(struct ufs_hba *hba)
+{
+	return 0;
+}
+#else
 static int ufshcd_wb_buf_flush_disable(struct ufs_hba *hba)
 {
 	int ret;
@@ -7146,7 +7194,14 @@ static int ufshcd_wb_buf_flush_disable(struct ufs_hba *hba)
 
 	return ret;
 }
+#endif
 
+#if 1
+static bool ufshcd_wb_is_buf_flush_needed(struct ufs_hba *hba)
+{
+	return false;
+}
+#else
 static bool ufshcd_wb_is_buf_flush_needed(struct ufs_hba *hba)
 {
 	int ret;
@@ -7220,6 +7275,7 @@ static bool ufshcd_wb_is_buf_flush_needed(struct ufs_hba *hba)
 	}
 	return false;
 }
+#endif
 
 /**
  * ufshcd_exception_event_handler - handle exceptions raised by device
