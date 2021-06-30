06586f4b4507fb240af3e09df8e3b1dfac9eda76
Arian <arian.kulmer@web.de>
2021-06-30 13:00:36 +0200

    input: touchscreen: goodix_driver_gt9886: Resume to change fod status
    * When using always on display the fod icon is shown after the touchscreen went into
      suspend state. Changing fod_status in suspended state throws the following error:
      "goodix_i2c 1-005d: Error applying setting, reverse things back"
    
    * To allow enabling fod when the touchscreen is already suspended we need to resume
      before enabling fod and suspend again.
    
    Change-Id: Ie44439fae0c8d71251aa802d2fe49212372e4b42

--- drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.c
+++ drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.c
@@ -2365,10 +2365,15 @@ static int gtp_set_cur_value(int gtp_mode, int gtp_value)
 	u8 temp_value = 0;
 	int ret = 0;
 	int i = 0;
+	bool suspended;
 
 	struct goodix_ts_device *dev = goodix_core_data->ts_dev;
 	ts_info("mode:%d, value:%d", gtp_mode, gtp_value);
 	if (gtp_mode == Touch_Fod_Enable && goodix_core_data) {
+		suspended = atomic_read(&goodix_core_data->suspended);
+		if (suspended) {
+			goodix_ts_resume(goodix_core_data);
+		}
 		goodix_core_data->fod_status = gtp_value;
 		if (goodix_core_data->fod_status == -1 || goodix_core_data->fod_status == 100) {
 			goodix_core_data->fod_enabled = false;
@@ -2378,6 +2383,9 @@ static int gtp_set_cur_value(int gtp_mode, int gtp_value)
 			goodix_core_data->fod_enabled = true;
 			goodix_core_data->gesture_enabled = goodix_core_data->double_wakeup |
 				goodix_core_data->fod_enabled | goodix_core_data->aod_status;
+		}
+		if (suspended) {
+			goodix_ts_suspend(goodix_core_data);
 		}
 		return 0;
 	}
