2c3c4cd0c367141b02f7aafc274f763d7e510dfc
Pranav Vashi <neobuddy89@gmail.com>
2021-10-09 09:29:43 +0530

    fdt: Modify commandline to set verifiedbootstate green
    Extracted from: https://github.com/vantoman/kernel_xiaomi_sm6150/commit/129edc8e5ab0ce92c74436f61942e9f031c35119
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -1261,6 +1261,15 @@ int __init early_init_dt_scan_chosen(unsigned long node, const char *uname,
 		} else {
 			strlcpy(cmdline, p, min((int)l, COMMAND_LINE_SIZE));
 		}
+		if (cmdline != NULL && *cmdline != '\0') {
+			char *q;
+			static char unlocked[] = "androidboot.verifiedbootstate=orange";
+			q = strstr(cmdline, unlocked);
+			if (q != NULL) {
+				memcpy(q + sizeof(unlocked) - 7, "green ", 6);
+				pr_info("Fixed security stuff in kernel cmdline from fdt\n");
+			}
+		}
 	}
 
 	pr_debug("Command line is: %s\n", (char*)data);
