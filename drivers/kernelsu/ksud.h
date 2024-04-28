61ec8eb7d2a947812b78e4c989333681eadb69d8
Lup Gabriel <gwolf2u.website@gmail.com>
2024-04-28 10:48:59 +0300

    drivers: Import KernelSU 0.9.3
    Change-Id: I285f3cfd87d79fd5fe235148983290a953dd1cb8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ drivers/kernelsu/ksud.h
@@ -0,0 +1,12 @@
+#ifndef __KSU_H_KSUD
+#define __KSU_H_KSUD
+
+#include <linux/types.h>
+
+#define KSUD_PATH "/data/adb/ksud"
+
+void on_post_fs_data(void);
+
+bool ksu_is_safe_mode(void);
+
+#endif
