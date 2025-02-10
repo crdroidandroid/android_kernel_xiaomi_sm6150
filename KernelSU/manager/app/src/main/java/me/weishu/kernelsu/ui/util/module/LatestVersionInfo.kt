38a371fff512554d74dbd9c41d26cc2132973deb
meloalfa159 <melolongo87@gmail.com>
2025-02-10 18:38:54 +0100

    remove kernelsu

--- KernelSU/manager/app/src/main/java/me/weishu/kernelsu/ui/util/module/LatestVersionInfo.kt
+++
@@ -1,7 +0,0 @@
-package me.weishu.kernelsu.ui.util.module
-
-data class LatestVersionInfo(
-    val versionCode : Int = 0,
-    val downloadUrl : String = "",
-    val changelog : String = ""
-)
--- KernelSU/website/docs/vi_VN/guide/hidden-features.md
+++
@@ -1,7 +0,0 @@
-# Tính Năng Ẩn
-
-## .ksurc
-
-Theo mặc định, `/system/bin/sh` tải `/system/etc/mkshrc`.
-
-Bạn có thể tạo su tải tệp rc tùy chỉnh bằng cách tạo tệp `/data/adb/ksu/.ksurc`.
