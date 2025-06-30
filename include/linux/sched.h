723c3339e112290d5c756c299bb5087368613931
Kir Kolyshkin <kolyshkin@gmail.com>
2025-06-30 17:51:38 +0300

    sched/headers: Move 'struct sched_param' out of uapi, to work around glibc/musl breakage

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -47,7 +47,6 @@ struct rcu_node;
 struct reclaim_state;
 struct robust_list_head;
 struct sched_attr;
-struct sched_param;
 struct seq_file;
 struct sighand_struct;
 struct signal_struct;
@@ -338,6 +337,10 @@ enum uclamp_id {
 	UCLAMP_MIN = 0,
 	UCLAMP_MAX,
 	UCLAMP_CNT
+};
+
+struct sched_param {
+	int sched_priority;
 };
 
 struct sched_info {
