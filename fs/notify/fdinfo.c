2a59ff0a328887b29f9295cb5caae8356548785b
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-07-21 18:36:02 +0800

    fs: susfs: merge and cherry-pick commits from susfs v1.5.9 gki-android14-6.1 branch
    https://gitlab.com/simonpunk/susfs4ksu/-/commit/bade6e818c21c88db546446a78d0ad500859b014
    https://gitlab.com/simonpunk/susfs4ksu/-/commit/75cdd2eadb0eba25362a1636edafcbd1fad44c21
    https://gitlab.com/simonpunk/susfs4ksu/-/commit/3e62ef841ce5e008cc6cc4618d0a37cedbb2f305
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/notify/fdinfo.c
+++ fs/notify/fdinfo.c
@@ -103,7 +103,7 @@ static void inotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark)
 	if (inode) {
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 		if (likely(susfs_is_current_non_root_user_app_proc()) &&
-				unlikely(inode->i_state & INODE_STATE_SUS_KSTAT)) {
+				unlikely(inode->i_mapping->flags & BIT_SUS_KSTAT)) {
 			struct path path;
 			char *pathname = kmalloc(PAGE_SIZE, GFP_KERNEL);
 			char *dpath;
