6ccb904b6cc86957e699ed6e1a36944dd5e7fcab
Pranav Vashi <neobuddy89@gmail.com>
2025-07-29 01:11:48 +0530

    drivers: Import KernelSU-Next 1.0.9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ drivers/kernelsu/throne_tracker.h
@@ -0,0 +1,12 @@
+#ifndef __KSU_H_UID_OBSERVER
+#define __KSU_H_UID_OBSERVER
+
+void ksu_throne_tracker_init();
+
+void ksu_throne_tracker_exit();
+
+void track_throne();
+
+bool is_lock_held(const char *path);
+
+#endif
