a89706d4e82c470493a882ac24d946931509751f
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-01-24 14:05:15 +0800

    fs: susfs: rename susfs_hide_sus_mnts_for_all_procs to susfs_hide_sus_mnts_for_non_su_procs
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -22,7 +22,8 @@
 #include "internal.h"
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool susfs_hide_sus_mnts_for_all_procs;
+extern bool susfs_hide_sus_mnts_for_non_su_procs;
+extern bool susfs_is_current_ksu_domain(void);
 #endif
 
 static unsigned mounts_poll(struct file *file, poll_table *wait)
@@ -110,7 +111,10 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_KSU_MNT_ID) {
+	if (susfs_hide_sus_mnts_for_non_su_procs &&
+			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
+			!susfs_is_current_ksu_domain())
+	{
 		return 0;
 	}
 #endif
@@ -152,7 +156,10 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_KSU_MNT_ID) {
+	if (susfs_hide_sus_mnts_for_non_su_procs &&
+			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
+			!susfs_is_current_ksu_domain())
+	{
 		return 0;
 	}
 #endif
@@ -222,7 +229,10 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_KSU_MNT_ID) {
+	if (susfs_hide_sus_mnts_for_non_su_procs &&
+			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
+			!susfs_is_current_ksu_domain())
+	{
 		return 0;
 	}
 #endif
