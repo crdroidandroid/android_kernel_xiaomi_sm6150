ab678a399a5e3b51c75a57bad80f7fe50a1164eb
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-02-12 19:31:35 +0800

    fs: susfs: Keep checking for ksu domain and assigning sus mnt_group_id persistently
    - Since mnt_alloc_group_id() will unlikely get called again after users unlock the device screen, and clone_mnt() rarely call mnt_alloc_group_id() to get new mnt_group_id as well, so basically it will not take any more resources after init and zygote process have fully initialized their jobs, and hence we can persistently check for new mounts mounted by ksu domain and assign sus mnt_group_id.
    - Also there is no need to use another ida for sus mnt_group_id, we can just re-use the default mnt_group_ida and use ida_alloc_min() to assign the sus mnt_group_id, so that we can as well remove the hook in mnt_release_group_id().
    
    co-authored by: simonpunk <simonpunk2016@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -38,7 +38,6 @@
 extern bool susfs_is_current_ksu_domain(void);
 extern bool susfs_is_sdcard_android_data_decrypted;
 
-static DEFINE_IDA(susfs_ksu_mnt_group_ida);
 static atomic64_t susfs_ksu_mounts = ATOMIC64_INIT(0);
 
 #define CL_COPY_MNT_NS BIT(25) /* used by copy_mnt_ns() */
@@ -152,8 +151,8 @@ static int mnt_alloc_group_id(struct mount *mnt)
 	 *   if it is ksu mounts, until susfs_is_sdcard_android_data_decrypted is set to true
 	 *   when boot-completed stage is triggered in core_hook.c 
 	 */
-	if (!susfs_is_sdcard_android_data_decrypted && mnt->mnt_id >= DEFAULT_KSU_MNT_ID) {
-		res = ida_alloc_min(&susfs_ksu_mnt_group_ida, DEFAULT_KSU_MNT_GROUP_ID, GFP_KERNEL);
+	if (susfs_is_current_ksu_domain()) {
+		res = ida_alloc_min(&mnt_group_ida, DEFAULT_KSU_MNT_GROUP_ID, GFP_KERNEL);
 		goto bypass_orig_flow;
 	}
 	res = ida_alloc_min(&mnt_group_ida, 1, GFP_KERNEL);
@@ -172,22 +171,6 @@ bypass_orig_flow:
  */
 void mnt_release_group_id(struct mount *mnt)
 {
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	/* - when boot-completed stage is triggered in core_hook.c,
-	 *   susfs_is_sdcard_android_data_decrypted will be set to true.
-	 * - Please note that if susfs_is_sdcard_android_data_decrypted is true, then
-	 *   it no longer checks for the sus mnt_group_id, and the allocated
-	 *   sus mnt_group_id will stay in kernel memory forever, and if user
-	 *   suddenly umounts the sus mount in global mnt namespace, the ida_free()
-	 *   function will throw error to kernel log, but it won't affect the system,
-	 *   so it is fine.
-	 */
-	if (!susfs_is_sdcard_android_data_decrypted && mnt->mnt_group_id >= DEFAULT_KSU_MNT_GROUP_ID) {
-		ida_free(&susfs_ksu_mnt_group_ida, mnt->mnt_group_id);
-		mnt->mnt_group_id = 0;
-		return;
-	}
-#endif
 	ida_free(&mnt_group_ida, mnt->mnt_group_id);
 	mnt->mnt_group_id = 0;
 }
