7e2226f89a1abd09e7b332ce9048f8ed36dcac9a
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-03-11 21:45:31 +0800

    fs: susfs: update sus_path and sus_path_loop to latest susfs 5.10 upstream (backport)
    Only modification for this is to use the `inode->i_state` instead of `inode->i_mapping->flags` as some features doesn't work on non-gki.
    
    reference: https://gitlab.com/simonpunk/susfs4ksu/-/commits/gki-android12-5.10?ref_type=heads
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -380,7 +380,9 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 		}
 #endif
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-		if (unlikely(inode->i_state & BIT_SUS_KSTAT)) {
+		if (unlikely(test_bit(AS_FLAGS_SUS_KSTAT, &inode->i_state) &&
+			susfs_is_current_proc_umounted_app()))
+		{
 			susfs_sus_ino_for_show_map_vma(inode->i_ino, &dev, &ino);
 			goto bypass_orig_flow;
 		}
@@ -906,8 +908,8 @@ static int show_smap(struct seq_file *m, void *v)
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
 	if (vma->vm_file &&
-		unlikely(file_inode(vma->vm_file)->i_state & BIT_SUS_MAPS) &&
-		susfs_is_current_proc_umounted())
+			unlikely(test_bit(AS_FLAGS_SUS_MAP, &file_inode(vma->vm_file)->i_state) &&
+			susfs_is_current_proc_umounted_app()))
 	{
 		show_map_vma(m, vma);
 		SEQ_PUT_DEC("Size:           ", vma->vm_end - vma->vm_start);
@@ -977,12 +979,14 @@ static int show_smaps_rollup(struct seq_file *m, void *v)
 
 	for (vma = priv->mm->mmap; vma; vma = vma->vm_next) {
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
-		if (vma->vm_file &&
-			unlikely(file_inode(vma->vm_file)->i_state & BIT_SUS_MAPS) &&
-			susfs_is_current_proc_umounted())
-		{
-			memset(&mss, 0, sizeof(mss));
-			goto bypass_orig_flow;
+		if (vma->vm_file) {
+			struct inode *inode = file_inode(vma->vm_file);
+			if (unlikely(test_bit(AS_FLAGS_SUS_MAP, &inode->i_state) &&
+				susfs_is_current_proc_umounted_app()))
+			{
+				memset(&mss, 0, sizeof(mss));
+				goto bypass_orig_flow;
+			}
 		}
 #endif
 		smap_gather_stats(vma, &mss);
@@ -1696,9 +1700,11 @@ static ssize_t pagemap_read(struct file *file, char __user *buf,
 		ret = walk_page_range(start_vaddr, end, &pagemap_walk);
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
 		vma = find_vma(mm, start_vaddr);
-		if (vma && vma->vm_file) {
+		if (vma->vm_file) {
 			struct inode *inode = file_inode(vma->vm_file);
-			if (unlikely(inode->i_state & BIT_SUS_MAPS) && susfs_is_current_proc_umounted()) {
+			if (unlikely(test_bit(AS_FLAGS_SUS_MAP, &inode->i_state) &&
+				susfs_is_current_proc_umounted_app()))
+			{
 				pm.show_pfn = false;
 				pm.buffer->pme = 0;
 			}
