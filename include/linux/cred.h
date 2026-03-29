bfa4245c733b659cf1d85870aedc99fc5b7b0048
Pranav Vashi <neobuddy89@gmail.com>
2026-03-29 20:52:11 +0530

    kernel: Add patch for fixing KernelPatch
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/cred.h
+++ include/linux/cred.h
@@ -29,6 +29,7 @@ struct inode;
  */
 struct group_info {
 	atomic_t	usage;
+	u32 _padding; // Fix for https://github.com/bmax121/APatch/issues/400
 	int		ngroups;
 	kgid_t		gid[0];
 } __randomize_layout;
@@ -109,7 +110,7 @@ extern void groups_sort(struct group_info *);
  * same context as task->real_cred.
  */
 struct cred {
-	atomic_long_t	usage;
+	atomic_t	usage;
 #ifdef CONFIG_DEBUG_CREDENTIALS
 	atomic_t	subscribers;	/* number of processes subscribed */
 	void		*put_addr;
@@ -226,7 +227,7 @@ static inline bool cap_ambient_invariant_ok(const struct cred *cred)
  */
 static inline struct cred *get_new_cred(struct cred *cred)
 {
-	atomic_long_inc(&cred->usage);
+	atomic_inc(&cred->usage);
 	return cred;
 }
 
@@ -270,7 +271,7 @@ static inline void put_cred(const struct cred *_cred)
 
 	if (cred) {
 		validate_creds(cred);
-		if (atomic_long_dec_and_test(&(cred)->usage))
+		if (atomic_dec_and_test(&(cred)->usage))
 			__put_cred(cred);
 	}
 }
