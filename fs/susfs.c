29f04d72549b0f292fa599ceaebed66b70f86973
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-05-03 22:14:37 +0800

    fs: susfs: Fix fuse path leaking from getdents/readdir syscall variants
    Prevent... kernel: Fix fuse path leaking from getdents/readdir syscall variants; Prevent dentry creation for fake_qstr
    
        - Since ilookup() cannot retrieve the inode from FUSE, so we use its underlying super_block from "/data" path instead.
    
        - If the looked up path is sus, its fs type is FUSE, the lookup flag is (LOOKUP_CREATE | LOOKUP_EXCL), and the target app uid has full file access granted, then at least we prevent the dentry creation for the fake_qstr, even though it is futile to do this, it is like better than doing nothing.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/susfs.c
+++ fs/susfs.c
@@ -24,6 +24,7 @@
 #include "mount.h"
 
 extern bool susfs_is_current_ksu_domain(void);
+extern void setup_selinux(const char *domain, struct cred *cred);
 
 #ifdef CONFIG_KSU_SUSFS_ENABLE_LOG
 bool susfs_is_log_enabled __read_mostly = true;
@@ -41,10 +42,6 @@ bool susfs_starts_with(const char *str, const char *prefix) {
     }
     return true;
 }
-
-#ifndef FUSE_SUPER_MAGIC
-#define FUSE_SUPER_MAGIC 0x65735546
-#endif
 
 /* sus_path */
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
@@ -85,6 +82,7 @@ void susfs_add_sus_path(void __user **user_info) {
 			goto out_path_put_path;
 		}
 		set_bit(AS_FLAGS_SUS_PATH, &fi->inode.i_state);
+		set_bit(AS_FLAGS_SUS_PATH, &inode->i_state);
 		SUSFS_LOGI("flagged AS_FLAGS_SUS_PATH on pathname: '%s', fi->nodeid: %llu, fi->inode.i_ino: %lu, fi->inode.i_state: 0x%lx\n", 
 					info.target_pathname, fi->nodeid, fi->inode.i_ino, fi->inode.i_state);
 		info.err = 0;
@@ -163,6 +161,7 @@ void susfs_run_sus_path_loop(void) {
 					continue;
 				}
 				set_bit(AS_FLAGS_SUS_PATH, &fi->inode.i_state);
+				set_bit(AS_FLAGS_SUS_PATH, &inode->i_state);
 				SUSFS_LOGI("re-flag AS_FLAGS_SUS_PATH on path '%s', fi->inode.i_ino: '%lu', fi->inode.i_state: 0x%lx\n",
 						cursor->target_pathname, fi->inode.i_ino, fi->inode.i_state);
 			} else {
@@ -180,6 +179,9 @@ static inline bool is_i_uid_not_allowed(uid_t i_uid) {
 	return likely(current_uid().val != i_uid);
 }
 
+/* - Please note that path inside /sdcard will be still visible to MediaProvider module,
+ *   since the uid of path like /sdcard/TWRP will be the uid of your MediaProvider module.
+ */
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 6, 0)
 bool susfs_is_inode_sus_path(struct mnt_idmap* idmap, struct inode *inode)
 #else
@@ -227,6 +229,10 @@ bool susfs_is_inode_sus_path(struct inode *inode)
 		return true;
 	}
 	return false;
+}
+
+int susfs_get_data_path(struct path *path) {
+	return kern_path("/data", LOOKUP_FOLLOW, path);
 }
 #endif // #ifdef CONFIG_KSU_SUSFS_SUS_PATH
 
@@ -1310,7 +1316,6 @@ out_copy_to_user:
 /* kthread for checking if /sdcard/Android is accessible via fsnoitfy */
 /* code is straightly borrowed from KernelSU's pkg_observer.c */
 #define SDCARD_ANDROID_PATH "/data/media/0/Android"
-extern void setup_selinux(const char *domain, struct cred *cred);
 bool susfs_is_sdcard_android_data_decrypted __read_mostly = false;
 
 struct watch_dir {
@@ -1449,7 +1454,7 @@ static int susfs_sdcard_monitor_fn(void *data)
 	commit_creds(cred);
 
 	if (!susfs_is_current_ksu_domain()) {
-		SUSFS_LOGE("domain is not su, exiting the thread\n");
+		SUSFS_LOGE("domain is not ksu, exiting the thread\n");
 		return -EINVAL;
 	}
 
--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -79,6 +79,10 @@
  
 #define MAGIC_MOUNT_WORKDIR "/debug_ramdisk/workdir"
 
+#ifndef FUSE_SUPER_MAGIC
+#define FUSE_SUPER_MAGIC 0x65735546
+#endif
+
 static inline bool susfs_is_current_proc_umounted(void) {
 	return test_ti_thread_flag(&current->thread_info, TIF_PROC_UMOUNTED);
 }
