986fad50539cabd8ad1fea2eda72320f7ecb039a
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-06-06 10:38:18 +0800

    kernelsu: lsm_hook: wire up setprocattr lsm hack for selinux_hide
    KSUN Added Changes:
    * add ksu_hlist_del_safe and ksu_list_del_safe from xxKSU
    
    sidex15's Note: this might be a messier one... but yeah it just works...
    
    Co-authored-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/kernelsu/hook/lsm_hooks.c
+++ drivers/kernelsu/hook/lsm_hooks.c
@@ -33,6 +33,41 @@ static int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
 }
 #endif
 
+extern int ksu_hide_setprocattr(const char *name, void *value, size_t size);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
+static int (*selinux_setprocattr_fn)(const char *name, void *value, size_t size) __read_mostly = NULL;
+static __nocfi int ksu_setprocattr_wrapper(const char *name, void *value, size_t size)
+{
+	ksu_hide_setprocattr(name, value, size);
+	if (likely(selinux_setprocattr_fn))
+		return selinux_setprocattr_fn(name, value, size);
+	return 0;
+}
+#else
+static int (*selinux_setprocattr_fn)(struct task_struct *p, char *name, void *value, size_t size) __read_mostly = NULL;
+static __nocfi int ksu_setprocattr_wrapper(struct task_struct *p, char *name, void *value, size_t size)
+{
+	ksu_hide_setprocattr(name, value, size);
+	if (likely(selinux_setprocattr_fn))
+		return selinux_setprocattr_fn(p, name, value, size);
+
+	return 0;
+}
+#endif
+
+/**
+ *  security_setprocattr is a weird LSM on 5.4 and up, and this is normally backported
+ *  down to 4.14 and 4.19. somehow this LSM is a one-shot. only the first to register
+ *  is called.
+ *
+ *  however this is not an issue for us on 3.x as we are hijacking selinux_ops on it
+ *
+ */
+#define SETPROCATTR_HOOK_NAME "ksu_setprocattr"
+static struct security_hook_list ksu_hooks_setprocattr[] __ro_after_init = {
+	LSM_HOOK_INIT(setprocattr, ksu_setprocattr_wrapper),
+};
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)
 static int ksu_inode_rename(struct mnt_idmap *idmap, struct inode *old_dir, struct dentry *old_dentry,
 			    struct inode *new_dir, struct dentry *new_dentry)
@@ -146,6 +181,239 @@ static const struct lsm_id ksu_lsmid = {
 };
 #endif
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
+static void ksu_hlist_del_safe(struct hlist_node *n)
+{
+	struct hlist_node *next = n->next;
+	struct hlist_node **pprev = n->pprev;
+
+	if (!pprev)
+		return;
+
+	// this is here so we don't get lost
+	/**
+	 *	original state
+	 * n			ptr	*ptr
+	 * H	hlist_head	0x1000	0xA000
+	 *
+	 * A	node->next	0xA000	0xB000
+	 *	node->pprev	0xA008	0x1000
+	 *
+	 * B	node->next	0xB000	0xC000
+	 *	node->pprev	0xB008	0xA000
+	 *
+	 * C	node->next	0xC000	0xFFFF
+	 *	node->pprev	0xC008	0xB000
+	 *
+	 */
+
+	// on hlist, pprev is the address of the 'next' pointer in the previous element
+	// so what we do is:
+	// 	write the value 0xC000 (next) into address 0xA000 (A->next)
+	// 	write the value 0xA000 (pprev) into address 0xC008 (C->pprev)
+
+	/**
+	 * 	after this routine
+	 *
+	 * H	hlist_head	0x1000	0xA000
+	 *
+	 * A	node->next	0xA000	0xC000  <-- now points to C
+	 *	node->pprev	0xA008	0x1000
+	 *
+	 * B	node->next	0xB000	0xC000  <-- orphaned
+	 *	node->pprev	0xB008	0xA000  <-- orphaned
+	 *
+	 * C	node->next	0xC000	0xFFFF
+	 *	node->pprev	0xC008	0xA000  <-- now points to A's next
+	 *
+	 */
+
+	// NOTE: pprev is **
+	uintptr_t addr = (uintptr_t)pprev;
+	uintptr_t base = addr & PAGE_MASK;
+	uintptr_t offset = addr & ~PAGE_MASK;
+
+	struct page *page = phys_to_page(__pa(base));
+	if (!page)
+		return;
+
+	// vmap pprev
+	void *writable_addr = vmap(&page, 1, VM_MAP, PAGE_KERNEL);
+	if (!writable_addr)
+		return;
+
+	uintptr_t target_slot = (uintptr_t)((uintptr_t)writable_addr + offset);
+
+	preempt_disable();
+	local_irq_disable();
+
+	WRITE_ONCE(*(struct hlist_node **)target_slot, next);
+
+	local_irq_enable();
+	preempt_enable();
+
+	vunmap(writable_addr);
+
+	smp_mb();
+
+	if (!next)
+		return;
+
+	// NOTE: pprev is **, taking ref, it becomes ***
+	addr = (uintptr_t)&next->pprev;
+	base = addr & PAGE_MASK;
+	offset = addr & ~PAGE_MASK;
+
+	page = phys_to_page(__pa(base));
+	if (!page)
+		return;
+
+	writable_addr = vmap(&page, 1, VM_MAP, PAGE_KERNEL);
+	if (!writable_addr)
+		return;
+
+	target_slot = (uintptr_t)((uintptr_t)writable_addr + offset);
+
+	preempt_disable();
+	local_irq_disable();
+
+	// use our pprev as the new pprev for the next in chain
+	WRITE_ONCE(*(struct hlist_node ***)target_slot, pprev);
+
+	local_irq_enable();
+	preempt_enable();
+
+	vunmap(writable_addr);
+
+	smp_mb();
+}
+
+static void ksu_dethrone_selinux_setprocattr()
+{
+	struct hlist_head *head = ksu_hooks_setprocattr[0].head; 
+	struct security_hook_list *pos;
+	struct hlist_node *tmp;
+
+	if (!head)
+		return;
+
+	hlist_for_each_entry_safe(pos, tmp, head, list) {
+
+		// grab selinux_setprocattr fn ptr
+		if (!strcmp(pos->lsm, "selinux")) {
+			selinux_setprocattr_fn = pos->hook.setprocattr;
+			pr_info("ksu_setprocattr: selinux_setprocattr found at 0x%lx \n", (uintptr_t)selinux_setprocattr_fn);
+		}
+
+		// remove everything else that aint us.
+		// NOTE: on some kernels BPF_LSM is enabled and it will also register setprocattr
+		// so this has to be done!
+		if (!!strcmp(pos->lsm, SETPROCATTR_HOOK_NAME)) {
+			pr_info("ksu_setprocattr: delete setprocattr LSM: %s\n", pos->lsm);
+			ksu_hlist_del_safe(&pos->list);
+		}
+	}
+}
+
+#else
+
+static void ksu_list_del_safe(struct list_head *entry)
+{
+	struct list_head *next = entry->next;
+	struct list_head *prev = entry->prev;
+
+	// on a linked list we have to patch both the before us and the next to us
+	if (!prev)
+		return;
+
+	// smash prev->next, basically we write 'next' into 'prev->next'
+	unsigned long addr_p = (unsigned long)&prev->next;
+	unsigned long base_p = addr_p & PAGE_MASK;
+	unsigned long offset_p = addr_p & ~PAGE_MASK;
+
+	struct page *page_p = phys_to_page(__pa(base_p));
+	if (!page_p)
+		return;
+
+	void *w_page = vmap(&page_p, 1, VM_MAP, PAGE_KERNEL);
+	if (!w_page)
+		return;
+
+	struct list_head **target = (void *)((unsigned long)w_page + offset_p);
+	
+	preempt_disable();
+	local_irq_disable();
+
+	WRITE_ONCE(*target, next);
+
+	local_irq_enable();
+	preempt_enable();
+
+	vunmap(w_page);
+	
+	smp_mb();
+
+	if (!next)
+		return;
+
+	// smash next->prev, basically we need to write 'prev' into 'next->prev'
+	unsigned long addr_n = (unsigned long)&next->prev;
+	unsigned long base_n = addr_n & PAGE_MASK;
+	unsigned long offset_n = addr_n & ~PAGE_MASK;
+
+	struct page *page_n = phys_to_page(__pa(base_n));
+	if (!page_n)
+		return;
+
+	w_page = vmap(&page_n, 1, VM_MAP, PAGE_KERNEL);
+	if (!w_page)
+		return;
+	
+	target = (void *)((unsigned long)w_page + offset_n);
+
+	preempt_disable();
+	local_irq_disable();
+
+	WRITE_ONCE(*target, prev);
+
+	local_irq_enable();
+	preempt_enable();
+
+	vunmap(w_page);
+
+	smp_mb();
+
+}
+
+static void ksu_dethrone_selinux_setprocattr()
+{
+	struct list_head *head = ksu_hooks_setprocattr[0].head;
+	struct security_hook_list *pos, *tmp;
+
+	if (!head)
+		return;
+
+	if (list_empty(head))
+		return;
+
+	list_for_each_entry_safe(pos, tmp, head, list) {
+		// dont unhook ourself!
+		if (pos->hook.setprocattr == ksu_setprocattr_wrapper)
+			continue;
+
+		// this is likely selinux_setprocattr, we save its address
+		if (!selinux_setprocattr_fn && pos->hook.setprocattr) {
+			selinux_setprocattr_fn = pos->hook.setprocattr;
+			pr_info("ksu_setprocattr: found first setprocattr at 0x%lx\n", (uintptr_t)selinux_setprocattr_fn);
+		}
+
+		// just delete evrything
+		pr_info("ksu_setprocattr: delete setprocattr LSM at 0x%lx\n", (uintptr_t)pos->hook.setprocattr);
+		ksu_list_del_safe(&pos->list);
+	}
+}
+#endif
+
 void __init ksu_lsm_hook_init(void)
 {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 8, 0)
@@ -157,6 +425,9 @@ void __init ksu_lsm_hook_init(void)
 	security_add_hooks(ksu_hooks, ARRAY_SIZE(ksu_hooks));
 #endif
 	pr_info("LSM hooks initialized.\n");
+	security_add_hooks(ksu_hooks_setprocattr, ARRAY_SIZE(ksu_hooks_setprocattr), SETPROCATTR_HOOK_NAME);
+	ksu_dethrone_selinux_setprocattr();
+	pr_info("setprocattr (SELinux Hide) hooks initialized.\n");
 }
 #else
 void __init ksu_lsm_hook_init(void)
