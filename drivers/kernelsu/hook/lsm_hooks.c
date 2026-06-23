9ef65cab7bd048959fcd137e789409148bf15c19
Pranav Vashi <neobuddy89@gmail.com>
2026-06-23 06:34:44 +0530

    Revert "kernelsu: fix incompatible pointer types in lsm_hooks"
    This reverts commit b27ad25bd41ad081568587dda278aadb52727787.

--- drivers/kernelsu/hook/lsm_hooks.c
+++ drivers/kernelsu/hook/lsm_hooks.c
@@ -389,17 +389,16 @@ static void ksu_list_del_safe(struct list_head *entry)
 
 static void ksu_dethrone_selinux_setprocattr()
 {
-	struct hlist_head *head = ksu_hooks_setprocattr[0].head;
-    struct security_hook_list *pos;
-    struct hlist_node *tmp;
+	struct list_head *head = ksu_hooks_setprocattr[0].head;
+	struct security_hook_list *pos, *tmp;
 
 	if (!head)
 		return;
 
-	if (hlist_empty(head))
+	if (list_empty(head))
 		return;
 
-	hlist_for_each_entry_safe(pos, tmp, head, list) {
+	list_for_each_entry_safe(pos, tmp, head, list) {
 		// dont unhook ourself!
 		if (pos->hook.setprocattr == ksu_setprocattr_wrapper)
 			continue;
@@ -412,7 +411,7 @@ static void ksu_dethrone_selinux_setprocattr()
 
 		// just delete evrything
 		pr_info("ksu_setprocattr: delete setprocattr LSM at 0x%lx\n", (uintptr_t)pos->hook.setprocattr);
-		hlist_del(&pos->list);
+		ksu_list_del_safe(&pos->list);
 	}
 }
 #endif
