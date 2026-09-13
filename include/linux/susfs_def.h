050522355ff9e2d02390e38938df0165648266ba
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-09-13 19:14:09 +0800

    fs: susfs: change DEFAULT_KSU_MNT_ID and DEFAULT_KSU_MNT_GROUP_ID to upstream values
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -43,9 +43,9 @@
 #define TRY_UMOUNT_DEFAULT 0 /* used by susfs_try_umount() */
 #define TRY_UMOUNT_DETACH 1 /* used by susfs_try_umount() */
 
-#define DEFAULT_KSU_MNT_ID 500000 /* used by mount->mnt_id */
+#define DEFAULT_KSU_MNT_ID 2000000000 /* used for mounts created or single cloned by ksu process */
 #define DEFAULT_SUS_MNT_ID_FOR_KSU_PROC_UNSHARE 1000000 /* used by vfsmount->susfs_mnt_id_backup */
-#define DEFAULT_KSU_MNT_GROUP_ID 5000 /* used by mount->mnt_group_id */
+#define DEFAULT_KSU_MNT_GROUP_ID 200000 /* used by mount->mnt_group_id */
 #define VFSMOUNT_MNT_FLAGS_KSU_UNSHARED_MNT 0x80000000 /* used for mounts that are unshared by ksu process */
 
 /*
