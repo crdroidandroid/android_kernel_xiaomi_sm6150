ead396425e09c4858b14a1c805fbac4e994cb591
simonpunk <simonpunk2016@gmail.com>
2026-09-13 18:52:56 +0800

    fs: susfs: SUS_MOUNT: Fix potential detection of mount minor dev gap - The detection was public by JingMatrix Demo app, see https://github.com/JingMatrix/Demo/commit/15e71acddb773b45b6c6821dfcc0e7eba85ec8f2
    - The fix here is to simply increase the default dev minor to a higer value "(1 << 12)" for all ksu mounts.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/super.c
+++ fs/super.c
@@ -36,8 +36,15 @@
 #include <linux/fsnotify.h>
 #include <linux/lockdep.h>
 #include <linux/user_namespace.h>
+#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+#include <linux/susfs_def.h>
+#endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 #include "internal.h"
 
+#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+extern bool susfs_is_current_ksu_domain(void);
+extern bool susfs_is_sdcard_android_data_decrypted;
+#endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 
 static LIST_HEAD(super_blocks);
 static DEFINE_SPINLOCK(sb_lock);
@@ -973,6 +980,20 @@ static DEFINE_IDA(unnamed_dev_ida);
 int get_anon_bdev(dev_t *p)
 {
 	int dev;
+
+#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+	if (!READ_ONCE(susfs_is_sdcard_android_data_decrypted) && susfs_is_current_ksu_domain()) {
+		dev = ida_alloc_range(&unnamed_dev_ida, DEFAULT_KSU_MNT_MINOR_DEV, (1 << MINORBITS) - 1,
+			GFP_ATOMIC);
+		if (dev == -ENOSPC)
+			dev = -EMFILE;
+		if (dev < 0)
+			return dev;
+
+		*p = MKDEV(0, dev);
+		return 0;
+	}
+#endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 
 	/*
 	 * Many userspace utilities consider an FSID of 0 invalid.
--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -83,6 +83,7 @@
 #define STATX_SUS_KSTAT_FUSE 0x20000000U
 
 #define MAGIC_MOUNT_WORKDIR "/debug_ramdisk/workdir"
+#define DEFAULT_KSU_MNT_MINOR_DEV (1 << 12) /* should be way enough, here minor(dev) begins with 4097 */
 
 #ifndef FUSE_SUPER_MAGIC
 #define FUSE_SUPER_MAGIC 0x65735546
